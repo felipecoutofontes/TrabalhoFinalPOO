bd3cac79f2daa61ff6b73111ed78ba077dc82cce
Julia Reis <juliafreisbittencourt@gmail.com>
2025-01-13 14:20:36 -0300

    ranking acessivel a partir do menu

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ import pygame as pg
 import sys
 import constants as c
 from telas import Screen
+from ranking import RankingScreen  # Importando a tela de Ranking
 
 # Função para desenhar texto
 def draw_text(text, font, color, surface, x, y):
@@ -20,17 +21,33 @@ class main_menu(Screen):
         self.BLUE = (0, 0, 255)
         self.font = pg.font.Font(None, 74)
         self.button_font = pg.font.Font(None, 50)
-        self.button_rect = pg.Rect(c.LARGURA_TOTAL // 2 - 100, c.SCREEN_HEIGHT // 2, 200, 50)
-        self.button_text = "Jogar"
+        
+        # Botão "Jogar"
+        self.play_button_rect = pg.Rect(c.LARGURA_TOTAL // 2 - 100, c.SCREEN_HEIGHT // 2, 200, 50)
+        self.play_button_text = "Jogar"
+
+        # Botão "Ranking"
+        self.ranking_button_rect = pg.Rect(c.LARGURA_TOTAL // 2 - 100, c.SCREEN_HEIGHT // 2 + 100, 200, 50)
+        self.ranking_button_text = "Ranking"
 
     def handle_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+            
+            # Verifica clique nos botões
             if event.type == pg.MOUSEBUTTONDOWN:
-                if self.button_rect.collidepoint(event.pos):
-                    self.running = False  # Interromper o loop do menu para iniciar o jogo
+                if self.play_button_rect.collidepoint(event.pos):
+                    self.running = False  # Interrompe o menu para iniciar o jogo
+                elif self.ranking_button_rect.collidepoint(event.pos):
+                    self.show_ranking()  # Abre a tela de ranking
+
+    def show_ranking(self):
+        ranking_screen = RankingScreen(self.screen)  # Cria a tela de ranking
+        while True:
+            if ranking_screen.run():
+                break  # Quando terminar a tela de ranking, volta para o menu
 
     def run(self):
         while self.running:  # Mantém o menu rodando enquanto ativo
@@ -44,6 +61,13 @@ class main_menu(Screen):
     def draw(self):
         self.screen.fill(self.WHITE)
         draw_text("Tower Defense", self.font, self.BLACK, self.screen, c.LARGURA_TOTAL // 2, c.SCREEN_HEIGHT // 4)
-        pg.draw.rect(self.screen, self.BLUE, self.button_rect)
-        draw_text(self.button_text, self.button_font, self.WHITE, self.screen, c.LARGURA_TOTAL // 2, c.SCREEN_HEIGHT // 2 + 25)
+
+        # Desenha o botão "Jogar"
+        pg.draw.rect(self.screen, self.BLUE, self.play_button_rect)
+        draw_text(self.play_button_text, self.button_font, self.WHITE, self.screen, c.LARGURA_TOTAL // 2, c.SCREEN_HEIGHT // 2 + 25)
+
+        # Desenha o botão "Ranking"
+        pg.draw.rect(self.screen, self.BLUE, self.ranking_button_rect)
+        draw_text(self.ranking_button_text, self.button_font, self.WHITE, self.screen, c.LARGURA_TOTAL // 2, c.SCREEN_HEIGHT // 2 + 125)
+
         pg.display.flip()
